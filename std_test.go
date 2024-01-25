f8fe772262d15ff3d3bb8e8dbd88fbec8f789f37
qiulaidongfeng <2645477756@qq.com>
2024-01-25 16:52:38 +0800

    秋来冬风：增加标准库arena测试

--- std_test.go
+++ std_test.go
@@ -20,3 +20,15 @@ func BenchmarkAlloc_int_stdArena(b *testing.B) {
 	runtime.GC()
 	debug.FreeOSMemory()
 }
+
+func BenchmarkAlloc_intAndFree_arena(b *testing.B) {
+	b.SetBytes(9)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		a := sa.NewArena()
+		for i := 0; i < 100; i++ {
+			sa.New[int](a)
+		}
+		a.Free()
+	}
+}
